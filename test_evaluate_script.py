b192e165ab7da1efaf00cc3e9170c25528796329
Jonathan Pilault <Jonathan.Pilault@gmail.com>
2024-05-21 23:08:24 +0000

    Initial commit

---
+++ test_evaluate_script.py
@@ -0,0 +1,26 @@
+from accelerate import Accelerator
+from datasets import load_dataset, load_metric
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer
+
+tokenizer = AutoTokenizer.from_pretrained("Zyphra/Zamba-v1")
+model = AutoModelForCausalLM.from_pretrained("Zyphra/Zamba-v1")
+
+# Initialize the accelerator
+accelerator = Accelerator()
+
+# Prepare the model, tokenizer, and data for distributed training
+model, tokenizer = accelerator.prepare(model, tokenizer)
+
+print(next(model.parameters()).device)
+# from transformers import pipeline
+
+# # Create a text generation pipeline with your fine-tuned model
+# generator = pipeline(task="text-generation", model=model, tokenizer=tokenizer)
+
+# # The prompt you want to generate text from
+# prompt = "Here is a sample prompt"
+
+# # Generate text 
+# generated_text = generator(prompt, max_length=100, num_return_sequences=1, temperature=0.7)[0]['generated_text']
+
+# print(generated_text)
